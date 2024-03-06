dd2d1abbee2b0068e776665b0e490c7bfe101377
Leon Kloker <leon@DN0a22b90d.SUNet>
2024-03-05 17:10:44 -0800

    meeting notes

--- model.py
+++ model.py
@@ -35,7 +35,8 @@ file = './data/19_populations.pickle'
 x, y = data.load_data(file)
 
 # Shuffle the data
-np.random.seed(0)
+random.seed(1)
+np.random.seed(1)
 xy = list(zip(x, y))
 random.shuffle(xy)
 x, y = zip(*xy)
@@ -53,7 +54,7 @@ max_combs = 2**13
 
 # Subsample the populations to get dataset
 x_train_mixy, y_train_mixy, x_test_mixy, y_test_mixy = data.subsample_populations_mixy(x, y, train_split=0.5, combine_train=True, combine_test=False, max_combs=max_combs)
-x_train_consty, y_train_consty, x_test_consty, y_test_consty = data.subsample_populations_consty(x, y, train_split=0.5, sample_size=0.8, combs_per_sample=int(max_combs/len(x)))
+x_train_consty, y_train_consty, x_test_consty, y_test_consty = data.subsample_populations_consty(x, y, train_split=0.6, sample_size=0.8, combs_per_sample=int(max_combs/len(x)))
 x_train_ = [*x_train_mixy, *x_train_consty]
 y_train = [*y_train_mixy, *y_train_consty]
 x_test_ = x_test_mixy
@@ -62,7 +63,7 @@ y_test = y_test_mixy
 # x_train_, y_train, x_test_, y_test = data.get_train_test_split(x, y, train_split=0.5, combine_train=True, combine_test=False)
 
 # Define statistical moment features
-features = ["mean"]#, "std", "skew", "kurt"]
+features = ["mean"] #, "std", "skew", "kurt"]
 
 # Define the features to use
 ifc_features = np.array([0,6,7,8,9,10,11,13,14,16])
@@ -71,8 +72,21 @@ ifc_features = np.array([0,6,7,8,9,10,11,13,14,16])
 x_train = data.get_statistical_moment_features(x_train_, features)[:, ifc_features]
 x_test = data.get_statistical_moment_features(x_test_, features)[:, ifc_features]
 
+# PCA of feature matrix, use coefficients as feature
+coef = np.array([])
+if len(coef) > 0:
+    U, S, V = np.linalg.svd(x_train.T, full_matrices=False)
+    pca_features_train = V[coef,:].T
+    pca_features_test = (np.diag(1/S) @ U.T @ x_test.T)[coef,:].T
+
+    # Add PCA features to the dataset
+    x_train = np.column_stack((x_train, pca_features_train))
+    x_test = np.column_stack((x_test, pca_features_test))
+
 # Weight the samples differently depending on activation
-weights = np.where(np.array(y_train) <= 0.1, 1, 1)
+w = 1
+print("Weighting samples with activation <= 0.1 by a factor of {}".format(w))
+weights = np.where(np.array(y_train) <= 0.1, w, 1)
 
 # Bin labels for classification
 bins = [0., 0.05, 0.20, 0.50, 1.]
