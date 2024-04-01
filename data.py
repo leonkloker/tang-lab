fixed cd203 normalization and binning, confusion matrix and implremented pdf features

data.py
@@ -14,15 +14,17 @@ def get_data(file, antigen="cd63+"):
     # Group by Baso population
     populations = df.groupby([antigen, 'patient_id'])
 
-    denominator = 100
+    # Set the denominator for the activation percentage
+    if "203" in antigen:
+        denominator = 1
+    else:
+        denominator = 100
 
     # Get activation percentage for each population
     y_raw = np.array(populations.mean().reset_index()[antigen]) / denominator
 
     samples = []
     for i, (_, population) in enumerate(populations):
-        if y_raw[i] > 1:
-            continue
         samples.append(population.iloc[:, :-2].to_numpy())
     return samples, y_raw
 
@@ -158,23 +160,33 @@ def bin(y, bins, verbose=False):
             print("{} samples with {} < y <= {}".format(np.sum(y == i+1), bins[i], bins[i+1]))
     return y
 
-# take a list of combined populations and return the marginal estimated pdf of each feature
+# take a list of populations and return the marginal estimated pdf of each feature
 # evaluated at the query points
-def get_marginal_distributions(features_list, query_points, method='kde'):
+def get_marginal_distributions(features_list, query_points, method='kde'):    
     results = []
-    n_features = features_list[0].shape[1]
+    n_features = len(features_list[0][0])
 
     for features in features_list:
         feature_distributions = []
+        features = np.array(features)
         
         for i in range(n_features):
             data = features[:, i]
             kde = scipy.stats.gaussian_kde(data)
             evaluated = kde.evaluate(query_points[i])
-            feature_distributions.append(evaluated)
+            feature_distributions = feature_distributions + list(evaluated)
 
         results.append(np.array(feature_distributions))
-    return results
+    return np.array(results)
+
+# takes a list of populations, finds mean and std of each feature across all populations 
+# and returns n_points equidistant query points up to n_std standard deviations around the mean
+def get_query_points_marginal(features_list, n_points=20, n_std=2):
+    features_single_cell = np.array([feature for features in features_list for feature in features])
+    mean = np.mean(features_single_cell, axis=0)
+    std = np.std(features_single_cell, axis=0)
+    query_points = np.transpose(np.linspace(mean - n_std*std, mean + n_std*std, n_points))
+    return query_points
 
 # Save the features and the activation rates to a file
 def save_data(file, x, y, combinations=False):
@@ -196,10 +208,20 @@ def load_data(file, combinations=False):
             return x, y
 
 if __name__ == "__main__":
-    antigen = "cd203c_norm"
+    antigen = "cd203c_MFI"
     file = './data/bat_ifc.csv'
     samples, y = get_data(file, antigen=antigen)
     samples = add_opacity(samples)
-    print(y)
-    save_data('./data/{}_populations_{}.pickle'.format(len(y), antigen), samples, y, combinations=False)
 
+    """ query_points = get_query_points_marginal(samples)
+    features = get_marginal_distributions(samples, query_points=query_points)
+    print(features[0].shape)
+    plt.figure()
+    for i in range(17):
+        plt.plot(features[0][i*20:(i+1)*20])
+    plt.show() """
+    """ plt.figure()
+    plt.plot(features[0][:20])
+    plt.show() """
+
+    #save_data('./data/{}_populations_{}.pickle'.format(len(y), antigen), samples, y, combinations=False)

evaluate_model.py
@@ -10,13 +10,12 @@ import matplotlib.pyplot as plt
 def plot_prediction(y, y_pred, savepath, title=None):
     plt.figure()
     plt.scatter(y, y_pred, label='MAE: {}'.format(title))
-    plt.scatter(y, y, marker='x', color='red')
+    plt.scatter(y, y, marker='.', color='red')
     plt.legend()
-    plt.plot([0, 1], [0, 1], color='red')
+    plt.plot([0, plt.gca().get_ylim()[1]], [0, plt.gca().get_ylim()[1]], color='red')
     plt.grid()
     plt.xlabel('Ground truth')
     plt.ylabel('Prediction')
-    plt.ylim(-0.2, 1.2)
     plt.savefig(savepath)
 
 # given the ground truth and the prediction,
@@ -25,7 +24,7 @@ def plot_confusion_matrix(y_true, y_pred, bins, savepath, labels=False):
     if not labels:
         y_pred = np.digitize(y_pred, bins=bins)
         y_true = np.digitize(y_true, bins=bins)
-    cm = confusion_matrix(y_true, y_pred)
+    cm = confusion_matrix(y_true, y_pred, labels=np.arange(1, len(bins)))
     
     disp = ConfusionMatrixDisplay(confusion_matrix=cm)
     disp.plot(cmap=plt.cm.Blues)

visualize_data.py
@@ -8,19 +8,13 @@ import umap
 import data
 
 # Read in the base populations
-antigen = "cd63+"
+antigen = "cd63"
 file = './data/20_populations_{}.pickle'.format(antigen)
 x, y = data.load_data(file)
 
-# Control the dataset size
-max_combs = 2**10
-
 # Subsample the populations to get dataset
-x_, y_, x_test_mixy, y_test_mixy = data.subsample_populations_mixy(x, y, train_split=.99, combine_train=False, combine_test=False, max_combs=max_combs)
+x_, y_, x_test_mixy, y_test_mixy = data.subsample_populations_mixy(x, y, train_split=.99, combine_train=False, combine_test=False)
 #x_train_consty, y_train_consty, x_test_consty, y_test_consty = data.subsample_populations_consty(x, y, train_split=.99, sample_size=0.8, combs_per_sample=int(max_combs/len(x)))
-
-# Define statistical moment features
-features = ["mean"] #, "std", "skew", "kurt"]
 
 # Define which frequencies to remove
 rm_freqs = []
@@ -38,6 +32,7 @@ for feature in ifc_features_baseline:
         ifc_features.append(feature)
 ifc_features = np.array(ifc_features)
 
+# Collect all cells in one matrix
 single_cells = np.array(x[0])
 single_cells_y = len(x[0])*[y[0]]
 for i in range(1, len(x)):
@@ -47,23 +42,33 @@ for i in range(1, len(x)):
 single_cells = single_cells[:, ifc_features]
 y = single_cells_y
 
+# Scale the single cell features
 scaler = StandardScaler()
 x = scaler.fit_transform(single_cells)
 
-# classify
+# Classify the cells by activation of their population
 bins = [0., 0.05, 0.20, 0.50, 1.]
 y = data.bin(y, bins)
 
 # UMAP
-reducer = umap.UMAP(n_components=2, n_neighbors=1000, min_dist=0.01, metric='euclidean', random_state=42)
-data_2d = reducer.fit_transform(x)
+reducer = umap.UMAP(n_components=3, metric='euclidean', random_state=42, n_neighbors=100, min_dist=0.01)
+umap_data = reducer.fit_transform(x)
 
-# Plot
+# plot 3d
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+scatter = ax.scatter(umap_data[:, 0], umap_data[:, 1], umap_data[:, 2], cmap='Spectral', s=5, c=y, alpha=0.3)
+ax.set_title('3D UMAP')
+colorbar = fig.colorbar(scatter, ax=ax, shrink=0.5, aspect=5)
+colorbar.set_label('Color scale label', rotation=270, labelpad=20)
+plt.show()
+plt.savefig("figures/umap_3d.png")
+
+""" # plot 2d
 plt.figure()
-plt.grid()
 plt.scatter(data_2d[:, 0], data_2d[:, 1], c=y, cmap='viridis', s=1, alpha=0.2)
 plt.title('Data visualization using UMAP')
 plt.xlabel('UMAP dimension 1')
 plt.ylabel('UMAP dimension 2')
 plt.colorbar(label='Class')
-plt.savefig("figures/umap.png")
+plt.savefig("figures/umap_2d.png") """